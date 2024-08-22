d7fcde16c83331ccc3ff00c7a2033ae577c1d3b5
Mohammad Abuzar Ansari <mohdabuzar909@gmail.com>
2024-08-22 21:48:07 +0530

    sitemap

---
+++ pages/pmpml-sitemap.xml.js
@@ -0,0 +1,44 @@
+//pages/sitemap.xml.js
+const EXTERNAL_DATA_URL = 'https://busroute.info/pmpml';
+
+function generateSiteMap(data) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     ${data
+       .map(path => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${path}`}</loc>
+           <lastmod>2024-07-25T09:03:46+00:00</lastmod>
+           <priority>0.80</priority>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(`https://api.busroute.info/api/v1/pmpml/routes_id`);
+  const data = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(data);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
